fc98c90cb09a597020184f77798510b467de833f
Thomas Laird <t6r6l5@gmail.com>
2018-01-22 19:31:23 -0700

    append color columns and fix exports.down

--- db/migrations/20180122141300_palettes.js
+++ db/migrations/20180122141300_palettes.js
@@ -2,10 +2,15 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('palettes', table => {
     table.increments('palette_id').primary();
     table.text('palette_name');
+    table.text('color1');
+    table.text('color2');
+    table.text('color3');
+    table.text('color4');
+    table.text('color5');
     table.integer('folder_id');
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('folders');
+  return knex.schema.dropTable('palettes');
 };
