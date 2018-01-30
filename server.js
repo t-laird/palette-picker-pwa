62f097740b4ceea485b15f5ebc74b399d4ebc1a2
Thomas Laird <t6r6l5@gmail.com>
2018-01-30 10:54:35 -0700

    sdf

--- server.js
+++ server.js
@@ -13,14 +13,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const requireHTTPS = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] != 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-    next();
-};
-
-
-const requireHTTPS = (req, res, next) => {
   if (req.header['x-forwarded-proto'] !== 'https') {
     return res.redirect('https://' + req.get('host') +req.url);
   }
